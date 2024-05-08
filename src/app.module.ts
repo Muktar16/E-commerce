d9dcf43ce10325a1464c88070177fc7cce72e094
Muktar16 <bsse1116@iit.du.ac.bd>
2024-05-08 16:04:50 +0600

    top level business domain modules created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { OrderModule } from './order/order.module';
+import { CartModule } from './cart/cart.module';
+import { ProductModule } from './product/product.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, OrderModule, CartModule, ProductModule, CommonModule],
 })
 
-export class AppModule {
-  constructor() {
-    console.log('AppModule created');
-  }
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(6000);
+  await app.listen(6000,()=>{
+    console.log('Server is running on http://localhost:6000')
+  });
 }
 bootstrap();
